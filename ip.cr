c92907d041cdc4ea0043c1d2674c260ddae742f0
Jake Meyer <jakewmeyer@gmail.com>
2018-10-03 10:29:26 -0500

    Add IP address validation

--- ip.cr
+++ ip.cr
@@ -1,12 +1,19 @@
 
 require "http/server"
 require "json"
+require "ipaddress"
 
 server = HTTP::Server.new do |context|
     if context.request.headers.has_key?("x-forwarded-for")
-        ip = {"ip" => context.request.headers["x-forwarded-for"].split(',')[0]}
-        context.response.content_type = "application/json"
-        context.response.print ip.to_json
+        begin
+            ip = IPAddress.new context.request.headers["x-forwarded-for"].split(',')[0]
+            output = {"ip" => ip.address}
+            context.response.content_type = "application/json"
+            context.response.print output.to_json
+        rescue ArgumentError
+            context.response.status_code = 404
+            context.response.print "Invalid IP address"
+        end
     else
         context.response.status_code = 404
         context.response.print "No IP address found"
