160d906d1a9b62e023e1558454a3c3522d485c56
Jake Meyer <jakewmeyer@gmail.com>
2018-10-03 13:36:30 -0500

    switch to kemal, and add plaintext route

--- src/public-ip-api.cr
+++ src/public-ip-api.cr
@@ -1,29 +1,50 @@
-
-require "http/server"
 require "json"
 require "ipaddress"
+require "kemal"
 
-server = HTTP::Server.new([
-  HTTP::ErrorHandler.new,
-  HTTP::CompressHandler.new,
-]) do |context|
-    if context.request.headers.has_key?("x-forwarded-for")
-        begin
-            ip = IPAddress.new context.request.headers["x-forwarded-for"].split(',')[0]
-            output = {"ip" => ip.address}
-            context.response.headers.add("Access-Control-Allow-Origin", "*")
-            context.response.content_type = "application/json"
-            context.response.print output.to_json
-        rescue ArgumentError
-            context.response.status_code = 404
-            context.response.print "Invalid IP address"
-        end
-    else
-        context.response.status_code = 404
-        context.response.print "No IP address found"
-    end
+Kemal.config.env = "production"
+serve_static false
+logging false
+
+error 404 do
+  ""
 end
 
-port = ENV["PORT"] ||= "5000"
-puts "Listening on port #{port}"
-server.listen("0.0.0.0", port.to_i, reuse_port: true)
+get "/" do |env|
+  if env.request.headers.has_key?("x-forwarded-for")
+    begin
+      env.response.content_type = "text/plain"
+      env.response.headers.add("Access-Control-Allow-Origin", "*")
+      (IPAddress.new env.request.headers["x-forwarded-for"].split(',')[0]).address
+    rescue ArgumentError
+      env.response.status_code = 404
+      env.response.print "Invalid IP address"
+    end
+  else
+    env.response.status_code = 404
+    env.response.print "No IP address found"
+  end
+end
+
+get "/json" do |env|
+    if env.request.headers.has_key?("x-forwarded-for")
+      begin
+        env.response.content_type = "application/json"
+        env.response.headers.add("Access-Control-Allow-Origin", "*")
+        {"ip" => (IPAddress.new env.request.headers["x-forwarded-for"].split(',')[0]).address}.to_json
+      rescue ArgumentError
+        env.response.status_code = 404
+        env.response.print "Invalid IP address"
+      end
+    else
+      env.response.status_code = 404
+      env.response.print "No IP address found"
+    end
+  end
+
+Kemal.run do |config|
+  port = ENV["PORT"] ||= "5000"
+  puts "Running on port #{port}"
+  server = config.server.not_nil!
+  server.bind_tcp "0.0.0.0", port.to_i, reuse_port: true
+end
